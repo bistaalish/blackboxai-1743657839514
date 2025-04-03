711c92a38940c3991b2fba0050bfb68419cd6120
Alish Bista <128159038+bistaalish@users.noreply.github.com>
2025-04-03 11:09:11 +0545

    Add OltOnuManager/navigation/RootNavigator.tsx 1743657851750

---
+++ OltOnuManager/navigation/RootNavigator.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/LoginScreen';
+import DashboardScreen from '../screens/DashboardScreen';
+
+export type RootStackParamList = {
+  Login: undefined;
+  Dashboard: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const RootNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Dashboard"
+          component={DashboardScreen}
+          options={{ title: 'OLT/ONU Manager' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigator;
